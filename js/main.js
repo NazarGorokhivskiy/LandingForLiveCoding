d2f8848807f0955c1fb07e60abe72920e6a5c873
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2020-06-23 21:51:45 +0300

    refactoring: rearranged project structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const OPEN_CLASSNAME = "open";
+
 const navLinks = document.getElementById("nav-links");
 
 function toggleMenu() {
-  if (!navLinks.classList.contains(OPEN_CLASSNAME)) {
-    navLinks.classList.add(OPEN_CLASSNAME);
-  } else {
+  if (navLinks.classList.contains(OPEN_CLASSNAME)) {
     navLinks.classList.remove(OPEN_CLASSNAME);
+  } else {
+    navLinks.classList.add(OPEN_CLASSNAME);
   }
 }
